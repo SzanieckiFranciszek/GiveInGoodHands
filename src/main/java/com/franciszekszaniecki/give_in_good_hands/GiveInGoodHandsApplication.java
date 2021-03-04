ea398f69c12e4659d0452b0d0b1886407072e0ba
Franciszek Szaniecki <franekszaniecki@gmail.com>
2021-03-04 12:14:17 +0100

    Initial commit

---
+++ src/main/java/com/franciszekszaniecki/give_in_good_hands/GiveInGoodHandsApplication.java
@@ -0,0 +1,13 @@
+package com.franciszekszaniecki.give_in_good_hands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveInGoodHandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveInGoodHandsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/franciszekszaniecki/give_in_good_hands/GiveInGoodHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciszekszaniecki.give_in_good_hands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveInGoodHandsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
